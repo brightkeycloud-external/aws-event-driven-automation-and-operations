84b28c9bca752cf0f7092abb940f52957f56166a
Chad Smith <chad@brightkeycloud.com>
2024-08-21 10:32:22 -0700

    typo fixes

--- 03/lambda_function.py
+++ 03/lambda_function.py
@@ -2,8 +2,7 @@ import boto3
 
 def lambda_handler(event, context):
 
-  regionid = event['region']
-  ec2 = boto3.resource("ec2", region_name=regionid)
+  ec2 = boto3.resource("ec2")
   available_volumes = ec2.volumes.filter(
     Filters=[{'Name': 'status', 'Values': ['available']}]
   )
